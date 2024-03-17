52e542bfdb97c2cb3b9d3213164c63679cf8ddb5
Ruchitha-Nagaraju <119487338+Ruchitha-Nagaraju@users.noreply.github.com>
2024-03-17 11:14:52 +0530

    Update Assignment.ino

--- Assignment.ino
+++ Assignment.ino
@@ -2,7 +2,6 @@
 TimeInterval blink;       //created the object blink 
 int baselineTemp = 0;  //initialization
 int celsius = 0;
-
 void setup()
 {
   pinMode(A0, INPUT);  //sensor's input pin
@@ -10,14 +9,12 @@ void setup()
   pinMode(2, OUTPUT);  // led output pin
   digitalWrite(2, LOW);  //initial state
 }
-
 void loop()
 {
   baselineTemp =30;
   celsius = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125); // conversion of fahrenheit to celsius
   Serial.print(celsius); //print output
   Serial.println("C ");
-
   //conditions as given
   if (celsius < baselineTemp) {
     if(blink.intervalMillis(250))  //function from TimeInterval library used for Time in milliseconds of the interval
@@ -25,7 +22,6 @@ void loop()
        digitalWrite(2,HIGH);
        digitalWrite(2,LOW);
       }
- 
   }
   else{
 if(blink.intervalMillis(500))   //function from TimeInterval library used for Time in milliseconds of the interval
